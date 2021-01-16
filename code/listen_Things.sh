21753e3742d0bef56bc65926d92223e16f9851b7
Byong-Wu Chong <andrewc76g@gmail.com>
2021-01-16 14:57:21 +0000

    Adding in other samples.

--- code/listen_Things.sh
+++ code/listen_Things.sh
@@ -1,4 +1,8 @@
 IOT_ENDPOINT=$(jq -r .endpointAddress < endpoint-setup.out)
 SCRIPT=~/environment/aws-iot-core-workshop/code/labBasicPubSub.py
 python ${SCRIPT} -e $IOT_ENDPOINT -r ./root-ca.pem -t "#" -m subscribe -w
+#python ${SCRIPT} -e $IOT_ENDPOINT -r ./root-ca.pem -t "\$aws/events/#" -m subscribe -w
+#python ${SCRIPT} -e $IOT_ENDPOINT -r ./root-ca.pem -t "\$aws/events/thing/+/created" -m subscribe -w
+#python ${SCRIPT} -e $IOT_ENDPOINT -r ./root-ca.pem -t "\$aws/provisioning-templates/#" -m subscribe -w
+#python ${SCRIPT} -e $IOT_ENDPOINT -r ./root-ca.pem -t "\$aws/certificates/#" -m subscribe -w
 
